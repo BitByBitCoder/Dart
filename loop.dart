0498cd04f73bf12fd77457a9c643eac28d51d703
BitByBitCoder <ahruaiaralte99@gmail.com>
2024-02-22 21:58:17 +0530

    map looping

--- loop.dart
+++ loop.dart
@@ -1,3 +1,12 @@
 void main() {
   print('for loop');
+
+  List<String> a = ['1', '2', '3'];
+
+  List b = a.map((e) {
+    if (e == '1') {
+      return e.toString();
+    }
+  }).toList();
+  print(b);
 }
