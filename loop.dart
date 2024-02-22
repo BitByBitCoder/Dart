22845ba8b5abde374951ce14b62518d604aba98e
BitByBitCoder <ahruaiaralte99@gmail.com>
2024-02-22 22:24:43 +0530

    map siam

--- loop.dart
+++ loop.dart
@@ -1,12 +1,30 @@
-void main() {
-  print('for loop');
+// void main() {
+//   print('for loop');
+//   var c = [
+//     {'name': 'hruaia', 'age': 23},
+//     {'name': 'hruaia', 'age': 23},
+//     {'name': 'hruaia', 'age': 23},
+//     {'name': 'hruaia', 'age': 23},
+//     {'name': 'hruaia', 'age': 23},
+//   ];
 
-  List<String> a = ['1', '2', '3'];
+//   // List<String> a = ['1', '2', '3'];
 
-  List b = a.map((e) {
-    if (e == '1') {
-      return e.toString();
-    }
-  }).toList();
-  print(b);
-}
+//   List b = c.map((e) {
+//     print("${e.keys} : ${e['name']}");
+//   }).toList();
+// }
+
+// class Person {
+//   String name;
+//   int age;
+
+//   Person({required this.name, required this.age});
+
+//   factory Person.fromJson(Map<String, dynamic> json) {
+//     return Person(name: json['name'], age: json['age']);
+//   }
+// }
+
+
+//for loop hi chu ka thiam sa deuh vek tawh
